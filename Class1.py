21a95f9b4085421af64911c20cc7ad7d4c32d149
polich7 <phominykh1997@gmail.com>
2019-09-29 17:10:14 +0500

    работает но не оч

--- Class1.py
+++ Class1.py
@@ -15,17 +15,43 @@ class Student (Group):
         return 'Student({!r},{!r},{!r},{!r})'.format(self.surname,self.name,self.middle_name,self.year)
     def __str__(self):
         return "{} {} {}".format(self.surname,self.name,self.middle_name) '''
+
+
+class Titul:
+    def __init__(self, university, faculty, years, group):
+        self.university = university
+        self.faculty = faculty
+        self.years = years
+        self.group = group
+
+    def __enter__(self):
+        print("{!r}\n{!r}\n{!r}\n{!r})\n \n ".format(self.university, self.faculty, self.years, self.group))
+
+class Students:
+    def __init__(self, number, surname, name, middle_name):
+        self.number = number
+        self.surname = surname
+        self.name = name
+        self.middle_name = middle_name
+
+
 class Lesson:
-    def __init__(self,name_subject,time,teacher,pages):
+    def __init__(self, name_subject, time, teacher, pages):
         self.name_subject = name_subject
         self.time = time
         self.teacher = teacher
         self.pages = pages
-class Journal(Lesson):
-    def __init__(self, name_subject,time,teacher,pages):
-        Lesson.__init__(self,name_subject,time,teacher,pages)
+
+
+class Journal(Lesson, Titul):
+    def __init__(self, university, faculty, years, group, name_subject, time, teacher, pages, number, surname, name, middle_name):
+        Titul.__init__(self, university, faculty, years, group)
+        Lesson.__init__(self, name_subject, time, teacher, pages)
+        Students.__init__(self, number, surname, name, middle_name)
+
     def __repr__(self):
-        return 'Lesson({!r},{!r},{!r},{!r})'.format(self.name_subject,self.time, self.teacher,self.pages)
+        return "{} {} {} {}".format(self.number, self.surname, self.name, self.middle_name)
+
     def __str__(self):
-        return "{} {} {} {}".format(self.name_subject,self.time, self.teacher,self.pages)
+        return "{} {} {} {}".format( self.name_subject, self.time, self.teacher, self.pages)
 
--- f1.py
+++ f1.py
@@ -1,10 +1,14 @@
 import Class1
 def test():
-    pr1 = Class1.Lesson
-    pr1.name_subject = "Высокоинтеллектуальные платформы"
-    pr1.time = 144
-    pr1.teacher = "Мартин Кютц"
-    pr1.pages = "8-9"
+    pr1 = Class1.Lesson("Высокоинтеллектуальные платформы", 144, "Мартин Кютц", "7-8")
+    pr3 = Class1.Lesson("Алгоритмические ЯП", 180, "Курушин", "9-10")
+    pr2 = Class1.Titul("Пермский национальный исследовательский политехнический университет", "Электортехнический факультет", "2019/2020", "АСУ2-19-1м")
+    pr4 = Class1.Students(1, "Фоминых", "Полина", "Юрьевна")
+    pr5 = Class1.Students(2, "Орлова", "Екатерина", "Дмитриевна")
+    print(Class1.Journal.__enter__(pr2))
     print(Class1.Journal.__str__(pr1))
+    print(Class1.Journal.__str__(pr3))
+    print(Class1.Journal.__repr__(pr4))
+    print(Class1.Journal.__repr__(pr5))
 if __name__ == '__main__':
     test()
