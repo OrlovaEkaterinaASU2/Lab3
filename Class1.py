df81c76599a7fcb62b763f60768c903904b50f07
lizaASU2 <eliza.veta13@yandex.ru>
2019-09-30 00:36:42 +0500

    работает но не оч 2.0

--- Class1.py
+++ Class1.py
@@ -1,22 +1,3 @@
-'''class Group():
-    def __init__(self, group_name, number, year):
-        self.group_name = group_name
-        self.number = number
-        self.year = year
-    #def __year(self):
-       # return self.year
-class Student (Group):
-    def __init__(self, name, surname, middle_name, number_group):
-        self.surname = surname
-        self.name = name
-        self.middle_name = middle_name
-        self.number_group = number_group
-    def __repr__(self):
-        return 'Student({!r},{!r},{!r},{!r})'.format(self.surname,self.name,self.middle_name,self.year)
-    def __str__(self):
-        return "{} {} {}".format(self.surname,self.name,self.middle_name) '''
-
-
 class Titul:
     def __init__(self, university, faculty, years, group):
         self.university = university
@@ -25,7 +6,7 @@ class Titul:
         self.group = group
 
     def __enter__(self):
-        print("{!r}\n{!r}\n{!r}\n{!r})\n \n ".format(self.university, self.faculty, self.years, self.group))
+        print("{!r}\n{!r}\n{!r}\n{!r} \n \n ".format(self.university, self.faculty, self.years, self.group))
 
 class Students:
     def __init__(self, number, surname, name, middle_name):
@@ -34,6 +15,8 @@ class Students:
         self.name = name
         self.middle_name = middle_name
 
+    def __enter__(self):
+        print("{!r}\n{!r}\n{!r}\n{!r} \n \n ".format(self.number, self.surname, self.name, self.middle_name))
 
 class Lesson:
     def __init__(self, name_subject, time, teacher, pages):
@@ -42,8 +25,10 @@ class Lesson:
         self.teacher = teacher
         self.pages = pages
 
+    def __enter__(self):
+        print("{!r}\n{!r}\n{!r}\n{!r} \n \n ".format(self.name_subject, self.time, self.teacher, self.pages))
 
-class Journal(Lesson, Titul):
+class Journal(Lesson, Titul,Students):
     def __init__(self, university, faculty, years, group, name_subject, time, teacher, pages, number, surname, name, middle_name):
         Titul.__init__(self, university, faculty, years, group)
         Lesson.__init__(self, name_subject, time, teacher, pages)
@@ -54,4 +39,3 @@ class Journal(Lesson, Titul):
 
     def __str__(self):
         return "{} {} {} {}".format( self.name_subject, self.time, self.teacher, self.pages)
-
