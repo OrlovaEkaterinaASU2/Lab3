6a1ba3fec20e50361dc16816e1efa8220fe5f293
OrlovaEkaterinaASU2 <orlova_ekaterina_97@mail.ru>
2019-09-29 16:05:02 +0500

    1 вариант

--- Class1.py
+++ Class1.py
@@ -1,10 +1,10 @@
-class Group():
+'''class Group():
     def __init__(self, group_name, number, year):
         self.group_name = group_name
         self.number = number
         self.year = year
-    def __year(self):
-        return self.year
+    #def __year(self):
+       # return self.year
 class Student (Group):
     def __init__(self, name, surname, middle_name, number_group):
         self.surname = surname
@@ -14,4 +14,18 @@ class Student (Group):
     def __repr__(self):
         return 'Student({!r},{!r},{!r},{!r})'.format(self.surname,self.name,self.middle_name,self.year)
     def __str__(self):
-        return "{} {} {}".format(self.surname,self.name,self.middle_name)
+        return "{} {} {}".format(self.surname,self.name,self.middle_name) '''
+class Lesson:
+    def __init__(self,name_subject,time,teacher,pages):
+        self.name_subject = name_subject
+        self.time = time
+        self.teacher = teacher
+        self.pages = pages
+class Journal(Lesson):
+    def __init__(self, name_subject,time,teacher,pages):
+        Lesson.__init__(self,name_subject,time,teacher,pages)
+    def __repr__(self):
+        return 'Lesson({!r},{!r},{!r},{!r})'.format(self.name_subject,self.time, self.teacher,self.pages)
+    def __str__(self):
+        return "{} {} {} {}".format(self.name_subject,self.time, self.teacher,self.pages)
+
--- f1.py
+++ f1.py
@@ -1,12 +1,10 @@
 import Class1
 def test():
-    gr1 = Class1.Group("ASU","2","19")
-    st1 = Class1.Student("Орлова", "Екатерина", "Дмитриевна","2")
-    st2 = Class1.Student("Фоминых", "Полина", "Юрьевна","2")
-    st2.year = "19"
-    st1.year = "19"
-    list_students = {1: st1, 2: st2}
-    for key in list_students.keys():
-        print(list_students[key].__repr__())
+    pr1 = Class1.Lesson
+    pr1.name_subject = "Высокоинтеллектуальные платформы"
+    pr1.time = 144
+    pr1.teacher = "Мартин Кютц"
+    pr1.pages = "8-9"
+    print(Class1.Journal.__str__(pr1))
 if __name__ == '__main__':
     test()
